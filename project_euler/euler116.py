bb00dc0e69d1659b8b19b5e34d1c0116ced76acc
Ritvik Mishra <ritvik@Ritviks-MacBook-Pro.local>
2014-08-29 16:24:50 -0400

    solved 116

---
+++ project_euler/euler116.py
@@ -0,0 +1,9 @@
+# generally, given the number of tiles and size of each cover
+def num_ways(tile_size, num_tiles):
+    ways = [1] * tile_size + [0] * (num_tiles - tile_size + 1)
+    for j in range(tile_size, num_tiles + 1):
+        ways[j] += ways[j - 1] + ways[j - tile_size]
+    return ways[num_tiles] - 1
+
+def solution(sizes, num_tiles):
+	return sum([num_ways(size, num_tiles) for size in sizes])
