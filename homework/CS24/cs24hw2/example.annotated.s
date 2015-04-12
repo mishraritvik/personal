a5fc53f36ed0f76ec90fa453ff03382578724646
Ritvik Mishra <mishraritvik@gmail.com>
2015-04-12 00:18:32 -0700

    started doing the set

---
+++ homework/CS24/cs24hw2/example.annotated.s
@@ -0,0 +1,32 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 10
+	.globl	_ex
+	.align	4, 0x90
+_ex:                                   ## @ex
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp0:
+	.cfi_def_cfa_offset 16
+Ltmp1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp2:
+	.cfi_def_cfa_register %rbp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	%edx, -12(%rbp)
+	movl	%ecx, -16(%rbp)
+	movl	-4(%rbp), %ecx
+	movl	-8(%rbp), %edx
+	subl	-12(%rbp), %edx            # subtraction is happening here
+	imull	%edx, %ecx                 # multiplication is happening here
+	addl	-16(%rbp), %ecx            # addition is happening here
+	movl	%ecx, -20(%rbp)
+	movl	-20(%rbp), %eax
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+
+.subsections_via_symbols
---
+++ homework/CS24/cs24hw2/factmain.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("Usage: %s [non-negative integer]\n", argv[0]);
+        return 0;
+    }
+
+    int n = atoi(argv[1]);
+
+    if (n < 0) {
+        printf("Argument must be a non-negative integer.");
+        return 0;
+    }
+
+    return fact(n);
+}
