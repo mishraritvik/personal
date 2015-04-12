4b4fa1f994c8af1fb5360a07bf0b7e51af62cf02
Ritvik Mishra <mishraritvik@gmail.com>
2015-04-12 15:17:34 -0700

    commented

--- homework/CS24/cs24hw2/gcd.s
+++ homework/CS24/cs24hw2/gcd.s
@@ -14,19 +14,18 @@
 gcd:
     pushl %ebp          /* Push old base pointer. */
     movl %esp, %ebp     /* Current stack is new base. */
-    cmpl $0, 12(%ebp)
-    movl 8(%ebp), %eax
-    jz gcd_return
+    cmpl $0, 12(%ebp)   /* Check if second argument is 0. */
+    movl 8(%ebp), %eax  /* Move first argument to eax. */
+    jz gcd_return       /* Return if second argument was 0. */
     // jnz gcd_resume
 
 gcd_resume:
-    xor %edx, %edx
-    // movl 8(%ebp), %eax
-    divl 12(%ebp)
-    movl 12(%ebp), %ebx
-    pushl %edx
-    pushl %ebx
-    call gcd
+    xor %edx, %edx        /* Set edx to 0. */
+    divl 12(%ebp)         /* Divide first by second to get remainder. */
+    movl 12(%ebp), %ebx   /* Move second argument to register. */
+    pushl %edx            /* Put remainder on stack as second argument. */
+    pushl %ebx            /* Put second argument on stack as first argument. */
+    call gcd              /* Make recursive call. */
     // jnp gcd_return
 
 gcd_return:
