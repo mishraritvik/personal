fc03ea484d635b4c0bd1f8f2f273ad1c92004177
Ritvik Mishra <mishraritvik@gmail.com>
2015-04-29 15:45:28 -0700

    better

--- homework/CS24/cs24hw4/exceptions/my_setjmp.s
+++ homework/CS24/cs24hw4/exceptions/my_setjmp.s
@@ -12,21 +12,21 @@ my_setjmp:
   mov   %esp, %ebp
 
   # move arg (mem location where execution state should go) into register
-  mov   8(%ebp), %edx
+  mov   8(%ebp), %eax
 
   # put stack pointer at first position in execution state memory block
-  mov   %esp, (%edx)
+  mov   %esp, (%eax)
 
   # put base pointer at second position in execution state memory block
   # have to move base pointer to register in order to move to memory
   mov   %ebp, %ecx
-  mov   %edx, 4(%edx)
+  mov   %edx, 4(%eax)
 
   # save all callee-saved registers in execution state memory block (pos 3 - 6)
-  mov   %ecx, 8(%edx)
-  mov   %ebx, 12(%edx)
-  mov   %esi, 16(%edx)
-  mov   %edi, 20(%edx)
+  mov   %ebx, 8(%eax)
+  mov   %ecx, 12(%eax)
+  mov   %esi, 16(%eax)
+  mov   %edi, 20(%eax)
 
   # Set eax (return val) to 0
   mov   $0, %eax
@@ -51,24 +51,24 @@ my_longjmp:
   mov   %esp, %ebp
 
   # get address of memory block into register
-  mov   8(%ebp), %edx
+  mov   8(%ebp), %eax
+
+  # put back stack pointer and return address
+  mov   (%eax), %esp
+  mov   4(%eax), %ebp
+
+  # put callee-saved registers values back to the original registers
+  mov   8(%eax), %ebx
+  mov   12(%eax), %ecx
+  mov   16(%eax), %esi
+  mov   20(%eax), %edi
 
   # set eax (return val) to 1 (if arg is 0) or n (if arg is n)
   # put 1 in edx because cmove needs registers, cannot give it $1
-  mov   $1, %ecx
+  mov   $1, %edx
   mov   12(%ebp), %eax
   test  $0, %eax
-  cmove %ecx, %eax
-
-  # put back stack pointer and return address
-  mov   (%edx), %esp
-  mov   4(%edx), %ebp
-
-  # put callee-saved registers values back to the original registers
-  mov   8(%edx), %ecx
-  mov   12(%edx), %ebx
-  mov   16(%edx), %esi
-  mov   20(%edx), %edi
+  cmove %edx, %eax
 
   # pop local stack and old base pointer
   mov   %ebp, %esp
