acbe49bdd299f806a55314d3316c105bd61ed17a
Ritvik Mishra <mishraritvik@gmail.com>
2015-05-21 00:02:11 -0700

    a

--- homework/CS24/cs24hw6/sthreads/test_arg.c
+++ homework/CS24/cs24hw6/sthreads/test_arg.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
 #include "sthread.h"
 
-int main(int argc, char **argv) {
+/**
+ * This function reads a counter variable, prints its value and indicates that
+ * it did the printing, then increments it and yields execution, finally
+ * returning when the counter variable is at least 6.
+ */
+static void thread1(void *arg) {
+    // Cast the argument to an int * so we can manipulate its value
+    int *counter = (int *) arg;
+    while (*counter < 6) {
+        printf("Counter = %d in thread 1, incrementing\n", *counter);
+        *counter += 1;
+        sthread_yield();
+    }
+}
 
+/**
+ * This function reads a counter variable, prints its value and indicates that
+ * it did the printing, then increments it and yields execution, finally
+ * returning when the counter variable is at least 6.
+ */
+static void thread2(void *arg) {
+    // Cast the argument to an int * so we can manipulate its value
+    int *counter = (int *) arg;
+    while (*counter < 6) {
+        printf("Counter = %d in thread 2, incrementing\n", *counter);
+        *counter += 1;
+        sthread_yield();
+    }
+}
+
+/**
+ * This program initializes a counter variable to 0, and then has two threads
+ * take turns printing its value and incrementing it, until they both return
+ * once it reaches a value of 6.
+ */
+int main(int argc, char **argv) {
+    int counter = 0;
+    sthread_create(thread1, (void *) &counter);
+    sthread_create(thread2, (void *) &counter);
+    sthread_start();
     return 0;
 }
--- homework/CS24/cs24hw6/sthreads/test_ret.c
+++ homework/CS24/cs24hw6/sthreads/test_ret.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "sthread.h"
 
 static void thread(void * arg) {
     int i, iter = *((int *) arg);
     for (i = 0; i < iter; ++i) {
-        printf("Iteration %d\n", i);
+        printf("Iteration %d of %d\n", i + 1, iter);
     }
 }
 
 int main(int argc, char **argv) {
     sthread_create(thread, (void *) 1);
     sthread_create(thread, (void *) 2);
-    sthread_create(thread, (void *) 3);
     sthread_create(thread, (void *) 4);
+    sthread_create(thread, (void *) 8);
     sthread_start();
     return 0;
 }
