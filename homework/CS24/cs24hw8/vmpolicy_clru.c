19d9bb979b95b8303b451c9f0f1c90563c50f826
Ritvik Mishra <mishraritvik@gmail.com>
2015-06-01 10:12:25 -0700

    debug

--- homework/CS24/cs24hw8/vmpolicy_clru.c
+++ homework/CS24/cs24hw8/vmpolicy_clru.c
@@ -175,8 +175,11 @@ void policy_timer_tick() {
             set_page_permission(curr_page, PAGEPERM_NONE);
 
             /* Remove from list and add again so it is at the end. */
-            remove_from_list(&pagelist, curr, prev);
-            add_page(&pagelist, curr_page);
+            prev->next = curr->next;
+            pagelist.tail->next = curr;
+            pagelist.tail = curr;
+            // remove_from_list(&pagelist, curr, prev);
+            // add_page(&pagelist, curr_page);
         }
 
         /* Move forward in list. */
