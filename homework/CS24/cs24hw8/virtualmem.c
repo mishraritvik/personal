5c76aa533cdc9c6cff27b5aa8cd5703fdf73b53d
Ritvik Mishra <mishraritvik@gmail.com>
2015-06-01 20:03:04 -0700

    a

--- homework/CS24/cs24hw8/virtualmem.c
+++ homework/CS24/cs24hw8/virtualmem.c
@@ -466,7 +466,7 @@ void map_page(page_t page, unsigned initial_perm) {
      */
 
     /* Seek. */
-    ret = lseek(fd_swapfile, page * PAGE_SIZE, SEEK_SET);
+    ret = lseek(fd_swapfile, page * PAGE_SIZE, (size_t) SEEK_SET);
 
     /* Check that it worked. */
     if (ret == -1) {
@@ -475,7 +475,7 @@ void map_page(page_t page, unsigned initial_perm) {
     }
 
     /* Read contents. */
-    ret = read(fd_swapfile, vm_address, PAGE_SIZE);
+    ret = read(fd_swapfile, vm_address, (size_t) PAGE_SIZE);
 
     /* Check that it worked. */
     if (ret == -1) {
@@ -540,6 +540,7 @@ void unmap_page(page_t page) {
         }
 
         /* Save to slot. */
+        set_page_permission(page, PAGEPERM_RDWR);
         ret = write(fd_swapfile, page_to_addr(page), PAGE_SIZE);
 
         /* Check that it worked. */
