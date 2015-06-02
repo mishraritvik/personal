e6ca8db77c013d9e6e66cc27eb86a89aa95d55ae
Ritvik Mishra <mishraritvik@gmail.com>
2015-06-01 17:03:29 -0700

    debug

--- homework/CS24/cs24hw8/virtualmem.c
+++ homework/CS24/cs24hw8/virtualmem.c
@@ -631,7 +631,9 @@ static void sigsegv_handler(int signum, siginfo_t *infop, void *data) {
         if (num_resident == max_resident) {
             page_t victim = choose_victim_page();
             assert(is_page_resident(victim));
+            printf("a\n");
             unmap_page(victim);
+            printf("b\n");
             assert(!is_page_resident(victim));
         }
 
