6f90a12cb9303e36d20d65a22b62b2931967ebd5
Ritvik Mishra <mishraritvik@gmail.com>
2015-06-01 19:58:16 -0700

    idiot

--- homework/CS24/cs24hw8/virtualmem.c
+++ homework/CS24/cs24hw8/virtualmem.c
@@ -475,7 +475,7 @@ void map_page(page_t page, unsigned initial_perm) {
     }
 
     /* Read contents. */
-    ret = read(fd_swapfile, page_to_addr(page), PAGE_SIZE);
+    ret = read(fd_swapfile, ret, PAGE_SIZE);
 
     /* Check that it worked. */
     if (ret == -1) {
